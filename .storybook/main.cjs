934f4d31cc5fc2d807efb0afa4fe7708688f1161
Chris Ellis <ellis.cs@gmail.com>
2021-11-08 13:41:35 +0000

    feat: add Tailwind to StoryBook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,42 @@
+const path = require('path');
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-svelte-csf',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				cssLoaderOptions: {
+					// When you have splitted your css over multiple files
+					// and use @import('./other-styles.css')
+					importLoaders: 1
+				},
+				postcssLoaderOptions: {
+					// When using postCSS 8
+					implementation: require('postcss')
+				}
+			}
+		}
+	],
 	svelteOptions: {
-		// "preprocess": require("../svelte.config.js").preprocess
+		// preprocess: require('../svelte.config.js').preprocess
+	},
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\,css&/,
+			use: [
+				{
+					loader: 'postcss-loader',
+					options: {
+						ident: 'postcss',
+						plugins: [require('tailwindcss'), require('autoprefixer')]
+					}
+				}
+			],
+			include: path.resolve(__dirname, '../')
+		});
+		return config;
 	}
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
+import '../src/app.css';
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/
+		}
+	}
+};
